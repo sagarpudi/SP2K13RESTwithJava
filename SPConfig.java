99609012878fdc5ad6be1a76463d086587e59291
Sagar Pudi <sagarpudi538@gmail.com>
2015-06-12 12:47:39 +0530

    Create SPConfig.java
    Holds the configuration details used in subsequent programs

---
+++ SPConfig.java
@@ -0,0 +1,11 @@
+package com.spudi.sp.config;
+
+public final class SPConfig {
+	
+	public static final String spSite="http://sp2k13:34596";
+	public static final String username="Administrator";
+	public static final String password="XYZ123$";
+	public static final String workstation="XYZWORKSTATION"; //JAVA_MACHINE_NAME
+	public static final String domain="XYZDOMAIN";	
+
+}
